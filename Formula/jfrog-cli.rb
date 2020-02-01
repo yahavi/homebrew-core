9ee486834fa382437563fd498a9a312c73b20a7d
yahavi <yahavi@jfrog.com>
2020-02-01 11:37:46 +0200

    jfrog-cli: renamed from jfrog-cli-go

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -1,15 +1,8 @@
-class JfrogCliGo < Formula
-  desc "Command-line interface for Jfrog Artifactory and Bintray"
+class JfrogCli < Formula
+  desc "Command-line interface for JFrog Artifactory and Bintray"
   homepage "https://github.com/jfrog/jfrog-cli"
-  url "https://github.com/JFrog/jfrog-cli-go/archive/1.33.2.tar.gz"
+  url "https://github.com/JFrog/jfrog-cli/archive/1.33.2.tar.gz"
   sha256 "b6682085d1ed19448e32bd208227229de43a85bb7e85653bc332c714ffaeb425"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "25271a36d26303283c11cd62500ca7d444b1890eb0982848eedda9a91ae629a7" => :catalina
-    sha256 "66b606d513c2520314b8e55eac2cf231fe727fa523336492c04cd53cc001feff" => :mojave
-    sha256 "dfa5eedd9e52effac52cf3b699906d58dca5c2da0610df5871681f8693659836" => :high_sierra
-  end
 
   depends_on "go" => :build
 
